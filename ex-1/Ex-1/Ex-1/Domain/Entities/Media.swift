93dfb9e5d54d7ad536ac3539dd1270b5ce43eaba
Bran <cold929@naver.com>
2022-12-25 22:38:41 +0900

    feat: Media Entity

---
+++ ex-1/Ex-1/Ex-1/Domain/Entities/Media.swift
@@ -0,0 +1,22 @@
+//
+//  Media.swift
+//  Ex-1
+//
+//  Created by Bran on 2022/12/25.
+//
+
+import Foundation
+
+struct Media: Equatable {
+  let name: String
+  let originName: String
+  let posterURL: String
+  let overview: String
+  let rating: Double
+}
+
+struct MediaPage: Equatable {
+  let page: Int
+  let totalPages: Int
+  let medias: [Media]
+}
