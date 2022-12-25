53a08de22a84a4ee9dddedfa2d60ec5c350639e6
Bran <cold929@naver.com>
2022-12-25 23:11:12 +0900

    feat: movie list item view

---
+++ ex-1/Ex-1/Ex-1/Presentation/MediaList/MovieListItemView.swift
@@ -0,0 +1,53 @@
+//
+//  MovieListItemView.swift
+//  Ex-1
+//
+//  Created by Bran on 2022/12/25.
+//
+
+import SwiftUI
+
+import Kingfisher
+
+struct MovieListItemView: View {
+  fileprivate let media: Media
+
+  init(media: Media) {
+    self.media = media
+  }
+
+  var body: some View {
+    HStack {
+      KFImage(URL(string: media.posterURL))
+        .resizable()
+        .frame(width: 80, height: 100)
+
+      VStack(alignment: .leading, spacing: 2) {
+        Text(media.name)
+          .font(.title3)
+
+        Text(media.overview)
+          .multilineTextAlignment(.leading)
+          .foregroundColor(.gray)
+          .font(.body)
+      }
+    }
+    .frame(height: 100)
+    .padding()
+  }
+}
+
+struct MovieListItemView_Previews: PreviewProvider {
+  static var previews: some View {
+    MovieListItemView(
+      media:
+        Media(
+          name: "웬즈데이",
+          originName: "Wednesday",
+          posterURL: "https://image.tmdb.org/t/p/original/tNWCukAMubqisamYURvo5jw61As.jpg",
+          overview: "똑똑하고 비꼬는 것에 도가 튼 웬즈데이 아담스. 암울함을 풍기는 그녀가 네버모어 아카데미에서 연쇄 살인 사건을 조사하기 시작한다. 새 친구도 사귀고, 앙숙도 만들며",
+          rating: 8.762
+        )
+    )
+  }
+}
