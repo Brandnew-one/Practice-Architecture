5897d28d7eba7bda5398df7ade828e1c2dab16c3
Bran <cold929@naver.com>
2023-01-07 16:04:09 +0900

    feat: rating double extension

---
+++ ex-1/Ex-1/Ex-1/Presentation/Extension/Double + Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Double + Extension.swift
+//  Ex-1
+//
+//  Created by Bran on 2023/01/07.
+//
+
+import Foundation
+
+extension Double {
+  var ratingType: String {
+    String(format: "%.1f", self)
+  }
+}
