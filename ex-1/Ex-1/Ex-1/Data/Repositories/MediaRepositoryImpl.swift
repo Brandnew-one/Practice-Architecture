d0db6aec5cb5b593ae296686775995c202b2352d
Bran <cold929@naver.com>
2023-01-08 17:58:17 +0900

    feat: repository impl in data layer

---
+++ ex-1/Ex-1/Ex-1/Data/Repositories/MediaRepositoryImpl.swift
@@ -0,0 +1,44 @@
+//
+//  MediaRepositoryImpl.swift
+//  Ex-1
+//
+//  Created by Bran on 2023/01/08.
+//
+
+import Combine
+import Foundation
+
+import Alamofire
+
+final class MediaRepositoryImpl {
+  // MARK: - DB
+//  private let dataBaseService: DataBaseService
+}
+
+extension MediaRepositoryImpl: MediaRepository {
+  func fetchTv(query: String, page: Int) -> AnyPublisher<MediaPage, Error> {
+    let requestDTO = MediaRequestDTO(query: query, page: page)
+
+    return NetworkService.shared.session
+      .request(MediaSearchRouter.searchTV(mediaRequestDTO: requestDTO))
+      .validate(statusCode: 200..<300)
+      .publishDecodable(type: MediaResponseDTO.self)
+      .value()
+      .map { $0.toDomain() }
+      .mapError { $0 as Error }
+      .eraseToAnyPublisher()
+  }
+
+  func fetchMovie(query: String, page: Int) -> AnyPublisher<MediaPage, Error> {
+    let requestDTO = MediaRequestDTO(query: query, page: page)
+
+    return NetworkService.shared.session
+      .request(MediaSearchRouter.searchMovie(mediaRequestDTO: requestDTO))
+      .validate(statusCode: 200..<300)
+      .publishDecodable(type: MediaResponseDTO.self)
+      .value()
+      .map { $0.toDomain() }
+      .mapError { $0 as Error }
+      .eraseToAnyPublisher()
+  }
+}
