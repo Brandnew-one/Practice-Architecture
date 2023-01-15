b882903658ac361442af9aeecff8b0515f98dc2a
Bran <cold929@naver.com>
2023-01-16 08:13:39 +0900

    feat: saved media list

---
+++ ex-1/Ex-1/Ex-1/Presentation/MediaList/Search/MediaListView.swift
@@ -0,0 +1,43 @@
+//
+//  MediaListView.swift
+//  Ex-1
+//
+//  Created by Bran on 2022/12/25.
+//
+
+import SwiftUI
+
+struct MediaListView: View {
+  @StateObject
+  var viewModel: MediaListViewModel
+
+  let appContainer: AppContainer
+
+  var body: some View {
+    NavigationView {
+      ScrollView {
+        LazyVStack {
+          ForEach(viewModel.output.medias, id: \.id) { media in
+            NavigationLink(
+              destination: {
+                NavigationLazyView(
+                  MediaDetailView(
+                    viewModel: appContainer.mediaDetailViewModel(media)
+                  )
+                )
+              },
+              label: {
+                MediaListItemView(media: media)
+              }
+            )
+          }
+        }
+        .padding(.top, 20)
+        .padding([.leading, .trailing], 12)
+      }
+      .navigationTitle("TV Search")
+      .navigationBarTitleDisplayMode(.inline)
+      .searchable(text: $viewModel.input.searchMediaSub.value)
+    }
+  }
+}
