afe30834a42d0e40364b0ac5a2adfb6b14e3620c
Bran <cold929@naver.com>
2023-04-05 22:09:35 +0900

    chore: project init

---
+++ DI-Test/DI-Test/DI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DI_TestApp.swift
+//  DI-Test
+//
+//  Created by Bran on 2023/04/05.
+//
+
+import SwiftUI
+
+@main
+struct DI_TestApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
