a4e67cd9851d36a65ba98ec6ad6f0250ae03dcf3
Bran <cold929@naver.com>
2023-04-12 21:58:59 +0900

    chore: project init

---
+++ TCA-01/TCA-01/TCA_01App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_01App.swift
+//  TCA-01
+//
+//  Created by Bran on 2023/04/12.
+//
+
+import SwiftUI
+
+@main
+struct TCA_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
