97fc21852e2d537491b797a21fd0b9d4b40b5990
Bran <cold929@naver.com>
2023-05-04 15:49:18 +0900

    chore: project init

---
+++ TCA-MovieSearch/TCA-MovieSearch/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TCA-MovieSearch
+//
+//  Created by Bran on 2023/05/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ TCA-MovieSearch/TCA-MovieSearch/TCA_MovieSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_MovieSearchApp.swift
+//  TCA-MovieSearch
+//
+//  Created by Bran on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct TCA_MovieSearchApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
